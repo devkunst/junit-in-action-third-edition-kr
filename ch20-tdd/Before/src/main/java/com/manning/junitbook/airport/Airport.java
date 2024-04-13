48a46e6633f077a94044cec2e4d94c34504e7c53
devkunst <devkunst@naver.com>
2024-04-14 06:11:49 +0900

    [chapter20] 20장 JUnit 5를 사용한 TDD
    - 20장 예제 한글화

--- ch20-tdd/Before/src/main/java/com/manning/junitbook/airport/Airport.java
+++ ch20-tdd/Before/src/main/java/com/manning/junitbook/airport/Airport.java
@@ -34,12 +34,12 @@ public class Airport {
         businessFlight.addPassenger(mike);
         economyFlight.addPassenger(mike);
 
-        System.out.println("Business flight passengers list:");
+        System.out.println("비즈니스 항공편 승객 리스트:");
         for (Passenger passenger : businessFlight.getPassengersList()) {
             System.out.println(passenger.getName());
         }
 
-        System.out.println("Economy flight passengers list:");
+        System.out.println("이코노미 항공편 승객 리스트:");
         for (Passenger passenger : economyFlight.getPassengersList()) {
             System.out.println(passenger.getName());
         }
