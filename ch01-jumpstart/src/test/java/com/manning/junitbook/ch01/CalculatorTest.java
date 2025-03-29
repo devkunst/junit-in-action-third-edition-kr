21f110dfef283d27636775e23d257a1f1246cd8b
devkunst <devkunst@naver.com>
2025-03-30 00:42:44 +0900

    [ch01] Maven 설정 제거 및 Gradle 전환, Java 17 적용
    - Maven pom.xml 설정 파일 삭제
    - Gradle build.gradle 기반으로 빌드 툴 통합
    - Java 17 버전 적용
    - JUnit jupiter 5.11.4 버전 적용

--- ch01-jumpstart/src/test/java/com/manning/junitbook/ch01/CalculatorTest.java
+++ ch01-jumpstart/src/test/java/com/manning/junitbook/ch01/CalculatorTest.java
@@ -20,9 +20,9 @@
  */
 package com.manning.junitbook.ch01;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class CalculatorTest {
 
