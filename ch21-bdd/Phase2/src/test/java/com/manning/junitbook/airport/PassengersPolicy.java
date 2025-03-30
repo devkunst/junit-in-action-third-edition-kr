20f42a74e4440eefa22687ee3d2f6a5edf0b2438
devkunst <devkunst@naver.com>
2025-03-31 00:03:56 +0900

    [ch21] Maven 설정 제거 및 Gradle 전환, Java 17 적용
    - Maven pom.xml 설정 파일 삭제
    - Gradle build.gradle 기반으로 빌드 툴 통합
    - Java 17 버전 적용

--- ch21-bdd/Phase2/src/test/java/com/manning/junitbook/airport/PassengersPolicy.java
+++ ch21-bdd/Phase2/src/test/java/com/manning/junitbook/airport/PassengersPolicy.java
@@ -20,9 +20,9 @@
  */
 package com.manning.junitbook.airport;
 
-import cucumber.api.java.en.Given;
-import cucumber.api.java.en.Then;
-import cucumber.api.java.en.When;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
 
 import java.util.ArrayList;
 
