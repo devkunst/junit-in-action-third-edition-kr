8d1f4c724c29c5e1cc85a7052b2575f11eecb704
devkunst <devkunst@naver.com>
2025-03-30 20:08:41 +0900

    [ch17] Maven 설정 제거 및 Gradle 전환, Java 17, Spring Boot 3.4.3 적용
    - Maven pom.xml 설정 파일 삭제
    - Gradle build.gradle 기반으로 빌드 툴 통합
    - Java 17 버전 적용
    - Spring Boot 3.4.3 적용

--- ch17-spring-boot/ch17-spring-boot-initializr-old-feature/src/test/java/com/manning/junitbook/springboot/RegistrationTest.java
+++ ch17-spring-boot/ch17-spring-boot-initializr-old-feature/src/test/java/com/manning/junitbook/springboot/RegistrationTest.java
@@ -27,7 +27,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
-
 import org.springframework.context.annotation.ImportResource;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
